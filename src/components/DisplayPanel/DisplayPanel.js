74d07e3b3ee5781c91b0d8c5b795b537d292c0e7
Hooman <hesamyan@outlook.com>
2020-06-03 02:09:05 +0400

    semi final round

--- src/components/DisplayPanel/DisplayPanel.js
+++ src/components/DisplayPanel/DisplayPanel.js
@@ -1,24 +1,65 @@
+/*
+****************   Please check lines 23-27   **********************************
+ */
+
+
 import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import NuGet from '../NuGet/NuGet'
 
 class DisplayPanel extends React.Component {
-    state = {searchField: '', pkg: '',type: '', major: false, minor: false, patch: false, error: null}
+    state = {mainURL: 'http://localhost:5000/', version: '-. - . - ', searchField: '', pkg: '',type: '', major: false,
+        minor: false, patch: false, error: null}
+
+    handleSearch = () => {
+        this.setState((state) => {
+            const {mainURL, pkg, type} = state;
+            const midURL = 'api/nuget/';
+            const suffixURL = '/json';
+            const queryURL = mainURL + midURL + pkg + '/' + type +  suffixURL;
+            fetch(queryURL)
+                .then(res => res.json()).then(result => {
+                    console.log("--fetch "+''.concat(result[2][0], ' . ', result[2][1], ' . ', result[2][2]));
+                    this.setState({version: ''.concat(result[2][0], ' . ', result[2][1], ' . ', result[2][2])}); // This works strangely!!
+                    // return ({version: ''.concat(result[2][0], ' . ', result[2][1], ' . ', result[2][2])}); // this did not work!!!
+            }, error => {
+                    this.setState({version: 'no feedback!'}); // This works strangely!!
+                // return ({version: 'No feedback!'}); // this did not work!!!
+            });
+            console.log("--state "+this.state.version);
+        });
+    }
+
+    handleSearchButton = (e) => {
+        e.preventDefault();
+        this.setState((state) => ({pkg: state.searchField}));
+        this.handleSearch();
+    }
+
+    handleSearchInput = (e) => {
+        this.setState({searchField: e.target.value});
+        this.setState({type: 'patch', major: false, minor: false, patch: true});
+    }
+
     render() {
-        const {searchField, pkg, type, major, minor, patch, error} = this.state;
+        const {version, pkg, type, major, minor, patch, error} = this.state;
+        let title;
+        if (pkg === '') {
+            title = <h5>Please input a Package name for searching.</h5>;
+        }
+        else {
+            title = <h5>Next version of " <strong>{pkg}</strong> " based on " {type} " :</h5>;
+        }
+
         return (<div>
                     <form action="">
                         <div className="form-row">
                             <div className="form-group col-md-6">
                                 <label htmlFor=""><h3>Search for packages on Nuget.org</h3></label>
                                 <hr/>
-                                <input type="text" className="form-control" placeholder="package" onChange={(e)=>
-                                {this.setState({searchField: e.target.value.toLowerCase()})}}/>
+                                <input type="text" className="form-control" placeholder="package" onChange={this.handleSearchInput}/>
                                 <br/>
-                                <button type='submit' className="btn btn-secondary btn-group-sm" onClick={(e) => {
-                                    e.preventDefault();
-                                    this.setState({pkg: searchField, type: 'major', major: true});
-                                }}>
+                                <button type='submit' className="btn btn-secondary btn-group-sm" onClick={this.handleSearchButton}>
                                     Search
                                 </button>
                             </div>
@@ -29,17 +70,23 @@ class DisplayPanel extends React.Component {
                             <div className="btn-group btn-group-vertical">
                                 <div className="form-check">
                                     <input className="form-check-input" type="radio" id="major"
-                                           onChange={(e)=>{this.setState({type: 'major', major: true, minor: false, patch: false})}} value='major' checked={major}/>
+                                           onChange={(e)=>{
+                                               this.setState({type: 'major', major: true, minor: false, patch: false});
+                                               this.handleSearch();}} value='major' checked={major}/>
                                     <label htmlFor="major" className="form-check-label">Major</label>
                                 </div>
                                 <div className="form-check">
                                     <input className="form-check-input" type="radio" id="minor"
-                                           onChange={(e)=>{this.setState({type: 'minor', major: false, minor: true, patch: false})}} name={type} value='minor' checked={minor}/>
+                                           onChange={(e)=>{
+                                               this.setState({type: 'minor', major: false, minor: true, patch: false});
+                                               this.handleSearch();}} name={type} value='minor' checked={minor}/>
                                     <label htmlFor="minor" className="form-check-label">Minor</label>
                                 </div>
                                 <div className="form-check">
                                     <input className="form-check-input" type="radio" id="patch"
-                                           onChange={(e)=>{this.setState({type: 'patch', major: false, minor: false, patch: true})}} name={type} value='patch' checked={patch}/>
+                                           onChange={(e)=>{
+                                               this.setState({type: 'patch', major: false, minor: false, patch: true});
+                                               this.handleSearch();}} name={type} value='patch' checked={patch}/>
                                     <label htmlFor="patch" className="form-check-label">Patch</label>
                                 </div>
                             </div>
@@ -48,10 +95,10 @@ class DisplayPanel extends React.Component {
                     <hr/>
                     <div className="card" style={{width: "35rem"}}>
                         <div className="card-header">
-                            <h5><strong>{pkg}</strong></h5>
+                            {title}
                         </div>
                         <div className="card-body">
-                            <NuGet package={pkg} versionType={type}/>
+                            <NuGet pkg={pkg} versionType={type} version={version}/>
                         </div>
                     </div>
                 </div>);
--- src/components/NuGet/NuGet.js
+++ src/components/NuGet/NuGet.js
@@ -3,44 +3,12 @@ import React from 'react';
 import './NuGet.css';
 
 export class NuGet extends React.Component {
-    state = { pkg: '' , version: '', type: ''};
-   
-    constructor(props) {
-        super(props)
-        this.state.pkg = props.package;
-        this.state.type = props.versionType;
-        console.log("--------------------");
-        console.log(this.props.pkg);
-        console.log("--------------------");
-    }
-    componentDidUpdate(prevProps) {
-        if (this.props.package !== prevProps.package || this.props.versionType !== prevProps.versionType) {
-            this.setState({
-                pkg: this.props.package,
-                type: this.props.versionType
-            });
-            const mainURL = 'http://localhost:5000/';
-            const midURL = 'api/nuget/';
-            const suffixURL = '/json';
-            const queryURL = mainURL + midURL + this.state.pkg + '/' + this.state.type +  suffixURL;
-
-            fetch(queryURL)
-                .then(res => res.json()).then(result => {
-                console.log(result);
-                this.setState({version: 'Next version: '+result[2][0]+' . ' +
-                        result[2][1]+' . '+result[2][2]+' based '+this.state.type});
-
-            }, error => {
-                console.log(error);
-                this.setState({version: 'No feedback!'});
-            })
-        }
-    }
     render() {
-        const  { version } = this.state;
-
+        const {pkg, versionType, version} = this.props;
         return (<div>
+                    <h6>{pkg}</h6>
                     <p>{version}</p>
+                    <p>{versionType}</p>
                 </div>);
     }
 }
